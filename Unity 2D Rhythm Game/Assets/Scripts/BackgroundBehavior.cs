f3b8c3392a0db748e8a5cac383860f6735046742
KimMolang <bluevill04@gmail.com>
2019-03-27 14:21:28 +0900

    [Completed] 3-2

---
+++ Unity 2D Rhythm Game/Assets/Scripts/BackgroundBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehavior : MonoBehaviour
+{
+    // TODO : https://educast.com/course/game/NZ46/lecture/37
+
+    public GameObject gameBackround;
+    private SpriteRenderer gameBackroundSpriteRenderer;
+
+    private void Start()
+    {
+        gameBackroundSpriteRenderer = gameBackround.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeOut(gameBackroundSpriteRenderer));
+    }
+
+    private IEnumerator FadeOut (
+        SpriteRenderer spriteRenderer
+        , float amount = 0.005f
+        , float timeGap = 0.005f )
+    {
+        Color color = spriteRenderer.color;
+
+        while(color.a > 0.0f)
+        {
+            color.a -= amount;
+            spriteRenderer.color = color;
+
+            yield return new WaitForSeconds(timeGap);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Unity 2D Rhythm Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; set; }
+
+    private void Awake()
+    {
+        if( instance == null )
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject); // gameObject 는 자기 자신
+        }
+    }
+
+
+    [Range(0.0f, 10.0f)]
+    public float noteSpeed = 0.05f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
